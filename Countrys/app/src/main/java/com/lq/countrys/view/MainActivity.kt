e413bc35903be12bc2ca5f9dd2344a8bfacb637f
cEgemen <cakirEgemen0@gmail.com>
2024-04-26 00:31:26 +0300

    feedFragment,specialCountryFragment and CountryRecycler layouts is completed in  country app

---
+++ Countrys/app/src/main/java/com/lq/countrys/view/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.lq.countrys.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.lq.countrys.R
+import com.lq.countrys.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var  navController : NavController ;
+    lateinit var  binding : ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+        navController = findNavController(R.id.fragmentContainerView)
+        NavigationUI.setupActionBarWithNavController(this,navController,null);
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null);
+    }
+
+}
